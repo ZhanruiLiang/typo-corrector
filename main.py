1b549e87c04fa80f166b91c73ce013612c7ca49f
Zhanrui <ray040123@gmail.com>
2022-09-29 17:29:23 -0700

    Dev (#1)
    * Create gitignore.
    
    * Change input/output params of main.py.
    Add main() to avoid leaking variables to global scope.
    Some minor styling changes.
    
    * Change input/output params of main.py.
    Add main() to avoid leaking variables to global scope.
    Some minor styling changes.
    
    * Delete duplicated output.

--- main.py
+++ main.py
@@ -2,11 +2,17 @@
 主要檢查嘅錯別字（錯字 -> 正字）：
 4. 係系喺
 5. 個 -> 嗰
+
+Example usage:
+
+  python3 main.py sample.txt
+  python3 main.py --outdir /tmp sample-0.txt sample-1.txt sample-2.txt
 """
 
 import argparse
+import pathlib
 import re
-from typing import List
+from typing import TextIO
 
 import pycantonese
 
@@ -21,12 +27,13 @@ cjk_regex = '[{}{}{}{}]'.format(han,
                                 full_width_punct, cjk_punct, kana, hangul)
 non_cjk_regex = '[^{}{}{}{}]'.format(
     han, full_width_punct, cjk_punct, kana, hangul)
+regular_typos: list[tuple[re.Pattern, str]] = []
 
 # Debugging purpose
 pos_file = open('pos.txt', 'w', encoding='utf-8')
 
 
-def segment_line(line: str) -> List[str]:
+def segment_line(line: str) -> list[str]:
     words = []
     segments = re.split("\s+", line)
     for seg in segments:
@@ -49,8 +56,8 @@ def fix_regular_typo(line: str) -> str:
     Regular typo means that they can be simply replaced by a regular expression.
     Fixing them does not require any context information.
     """
-    for r in regular_typos:
-        line = re.sub(r[0], r[1], line)
+    for typo, replace in regular_typos:
+        line = typo.sub(replace, line)
     return line
 
 
@@ -154,23 +161,37 @@ def fix_contextual_typo(line: str) -> str:
     return line
 
 
-if __name__ == "__main__":
+def correct(input: TextIO, output: TextIO):
+    """Corrects typos from input file and write to output file."""
+    for line in input:
+        fixed = fix_regular_typo(line.strip())
+        fixed = fix_contextual_typo(fixed)
+        fixed = fix_space(fixed)
+        output.write(fixed + "\n")
+
+
+def main():
     parser = argparse.ArgumentParser(description='Fix Cantonese typo.')
     parser.add_argument(
-        '--input', type=str, default="input.txt", help='Input text file, each line is a sentence.')
+        'inputs', type=str, nargs='+',
+        help='Input text file, each line is a sentence.')
+    parser.add_argument(
+        'outdir', type=str, default="output", nargs='?',
+        help='Output directory.')
     args = parser.parse_args()
 
     # Read regular typos
-    regular_typos = []
-    lines = open('regular.txt', 'r', encoding='utf-8').readlines()
-    for line in lines:
-        pair = line.strip().split(',')
-        regular_typos.append((re.compile(pair[0]), pair[1]))
+    for line in open('regular.txt', 'r', encoding='utf-8'):
+        typo, replace = line.strip().split(',')
+        regular_typos.append((re.compile(typo), replace))
 
-    output = open("output.txt", "w", encoding="utf-8")
-    with open(args.input, 'r', encoding='utf-8') as f:
-        for line in f:
-            fixed_regular_line = fix_regular_typo(line.strip())
-            fixed_contextual_line = fix_contextual_typo(fixed_regular_line)
-            fixed_line = fix_space(fixed_contextual_line)
-            output.writelines(fixed_line + "\n")
+    outdir = pathlib.Path(args.outdir)
+    outdir.mkdir(parents=True, exist_ok=True)
+    for input in map(pathlib.Path, args.inputs):
+        output = outdir / input.name
+        with (open(input, 'r', encoding='utf-8') as input_f, 
+              open(output, 'w', encoding='utf-8') as output_f):
+            correct(input_f, output_f)
+
+if __name__ == "__main__":
+    main()
