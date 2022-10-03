87b04f6061adb71fdeec3c8ca7ea86bc4e462951
Zhanrui <ray040123@gmail.com>
2022-10-02 22:32:37 -0700

    Migrate rules in main.py to rules.py. (#3)
    * Add all rules from main.py into rules.py.
    
    * Fix bugs in main.py.
    
    * Fix bug in apply_contextual_rules.
    
    * Fix bugs in rules and update sample output file.
    
    * Remove old rules.
    
    * Fix typing in main.py

--- main.py
+++ main.py
@@ -14,37 +14,7 @@ import pycantonese
 
 import rules
 
-
-han = r'\u3006\u3007\u4e00-\u9fff\u3400-\u4dbf\U00020000-\U0002a6df\U0002a700-\U0002ebef\U00030000-\U0003134f'
-full_width_punct = r'\uFF00-\uFFEF'
-cjk_punct = r'\u3000-\u303F'
-kana = r'\u3040-\u309f\u30a0-\u30ff\u31F0-\u31FF'
-hangul = r'\uAC00-\uD7AF\u1100-\u11ff'
-
-cjk_regex = f'[{han}{full_width_punct}{cjk_punct}{kana}{hangul}]'
-non_cjk_regex = f'[^{han}{full_width_punct}{cjk_punct}{kana}{hangul}]'
 regular_typos: list[tuple[re.Pattern, str]] = []
-
-# Debugging purpose
-pos_file = open('pos.txt', 'w', encoding='utf-8')
-
-
-def segment_line(line: str) -> list[str]:
-    words = []
-    segments = re.split("\s+", line)
-    for seg in segments:
-        words += pycantonese.segment(seg)
-    return words
-
-
-def fix_space(line: str) -> str:
-    """
-    Remove spaces between Han characters and non-Han characters.
-    """
-    cjk_pattern = '(?<={})\s+(?={})'.format(cjk_regex, cjk_regex)
-    line = re.sub(cjk_pattern, '', line)
-    number_pattern = '(?<={})\s+(?={})'.format(r'[\d.,]', r'[\d.,]')
-    return re.sub(number_pattern, '', line)
 
 
 def fix_regular_typo(line: str) -> str:
@@ -57,120 +27,13 @@ def fix_regular_typo(line: str) -> str:
     return line
 
 
-def fix_contextual_typo(line: str) -> str:
-    """
-    Contextual typo means that they can be fixed by using the context information.
-
-    List of typos:
-    1. 係系喺
-    2. 個 -> 嗰
-    3. 咁 -> 噉
-    4. 無 -> 冇
-    5. D -> 啲
-    6. 比 -> 畀
-    7. 既 -> 嘅
-    8. 左 -> 咗
-    """
-
-    pos_list = pycantonese.pos_tag(segment_line(line))
-
-    # Debugging purpose
-    print(pos_list, file=pos_file)
-
-    for i, pair in enumerate(pos_list):
-        word, pos = pair
-        length = len(pos_list)
-        prev_pos = pos_list[i - 1][1] if i > 0 else ''
-        next_word = pos_list[i + 1][0] if i < length - 1 else ''
-        next_pos = pos_list[i + 1][1] if i < length - 1 else ''
-
-        # 左 -> 咗
-        # 如果 左 字前面係一個動詞，噉就改成 咗
-        if "左" in word:
-            if prev_pos == "VERB":
-                pos_list[i] = (word.replace("左", "咗"), pos)
-        # 既 -> 嘅
-        # 如果 既 字前面係一個名詞/動詞/形容詞/副詞，句子後面又冇"又 ADV/ADJ/VERB"嘅結構，噉就改成 嘅
-        elif word == "既":
-            if i >= 1 and pos_list[i-1][1] in ["PRON", "NOUN", "ADJ", "ADV", "VERB"]:
-                # 句子後面冇 "又 ADV/ADJ/VERB" 嘅結構
-                if "又" in "".join([pair[0] for pair in pos_list[i:]]) and next_pos not in ["ADJ", "ADV", "VERB"]:
-                    pass
-                else:
-                    pos_list[i] = ("嘅", pos)
-        # 黎 -> 嚟
-        # 如果 黎 字係動詞，就改成 嚟
-        elif word == "黎" and pos == "VERB":
-            pos_list[i] = ("嚟", pos)
-        # 野 -> 嘢
-        # 如果係隻名詞，就改成 嘢
-        # 包埋動詞同X係因為 pycantonese 有時會識別成動詞
-        elif word == "野":
-            if pos in ["NOUN",  "X", "PRON"]:
-                pos_list[i] = ("嘢", pos)
-            elif prev_pos in ["VERB"]:
-                pos_list[i] = ("嘢", pos)
-        # 咁/甘 -> 噉, 甘 -> 咁
-        # 如果前面係形容詞、副詞，或者後面後動詞、名詞、代詞，就係 噉
-        # 如果後面係形容詞、副詞，就係 咁
-        elif word == "咁" or (word == "甘" and pos not in ["VERB", "NOUN"]):
-            if i == length - 1:
-                pos_list[i] = ("噉", pos)
-            else:
-                if next_pos in ["ADJ", "ADV"]:
-                    pos_list[i] = ("咁", pos)
-                elif next_pos in ["VERB", "NOUN", "PRON", "PART", "AUX"] or (prev_pos in ["ADJ", "ADV"]):
-                    pos_list[i] = ("噉", pos)
-                elif prev_pos in ["ADJ", "ADV"]:
-                    pos_list[i] = ("噉", pos)
-        # 比 -> 畀
-        # 如果後面第一個詞係名詞，且第二個詞係形容詞、副詞，就係 比
-        elif word == "比":
-            remain_words = [pair[0] for pair in pos_list[i:]]
-            remain_pos = [pair[1] for pair in pos_list[i:]]
-            if "仲" in remain_words or "更" in remain_words or "ADJ" in remain_pos or "ADV" in remain_pos:
-                pass
-            else:
-                pos_list[i] = ("畀", pos)
-        # 俾 -> 畀
-        elif word == "俾":
-            pos_list[i] = ("畀", pos)
-        # 個 -> 嗰
-        # 係系喺
-        elif word == "曬":
-            if prev_pos == "VERB":
-                pos_list[i] = ("晒", pos)
-        # 無 -> 冇
-        elif word == "無":
-            if i <= length-2 and pos_list[i+1][1] in ["NOUN", "ADP"]:
-                pos_list[i] = ("冇", pos)
-        # d/D -> 啲
-        elif word in ["d", "D"]:
-            if re.compile(cjk_regex).search(next_word):
-                pos_list[i] = ("啲", pos)
-            elif prev_pos in ["ADJ", "ADV"]:
-                pos_list[i] = ("啲", pos)
-        # 以下詞嘅修復需要用到句子詞性同上下文信息
-        elif word == "宜家":
-            if any(word in line for word in ["傢俬", "傢俱", "家居"]):
-                pass
-            else:
-                pos_list[i] = (word.replace("宜家", "而家"), pos)
-
-    line = " ".join([pair[0] for pair in pos_list])
-
-    return line
-
-
 def correct(input: TextIO, output: TextIO) -> None:
     """
     Corrects typos from input file and write to output file.
     """
     for line in input:
         fixed = fix_regular_typo(line.strip())
-        fixed = fix_contextual_typo(fixed)
-        # fixed = rules.apply_contextual_rules(fixed)
-        fixed = fix_space(fixed)
+        fixed = rules.apply_contextual_rules(fixed)
         output.write(fixed + "\n")
 
 
--- rules.py
+++ rules.py
@@ -4,6 +4,15 @@ from typing import Callable
 
 import pycantonese
 
+han = r"\u3006\u3007\u4e00-\u9fff\u3400-\u4dbf\U00020000-\U0002a6df\U0002a700-\U0002ebef\U00030000-\U0003134f"
+full_width_punct = r"\uFF00-\uFFEF"
+cjk_punct = r"\u3000-\u303F"
+kana = r"\u3040-\u309f\u30a0-\u30ff\u31F0-\u31FF"
+hangul = r"\uAC00-\uD7AF\u1100-\u11ff"
+
+cjk_regex = f"[{han}{full_width_punct}{cjk_punct}{kana}{hangul}]"
+cjk_pattern = re.compile(rf"(?<={cjk_regex})\s+(?={cjk_regex})")
+number_pattern = re.compile(r"(?<={num})\s+(?={num})".format(num=r"[\d.,]"))
 
 class Context:
     pos_list: list[tuple[str, str]]
@@ -17,10 +26,8 @@ class Context:
 
     @property
     def sentence_remain(self):
-        """
-        The remaining part of the sentence.
-        """
-        return ''.join(x[0] for x in self.pos_list[self.i:])
+        """The remaining part of the sentence."""
+        return "".join(x[0] for x in self.pos_list[self.i:])
 
     def replace_word(self, word: str):
         self.pos_list[self.i] = word, self.pos_list[self.i][1]
@@ -32,17 +39,58 @@ class Pos:
     VERB = "VERB"
     PRON = "PRON"
     ADJ = "ADJ"
+    ADP = "ADP"
     ADV = "ADV"
+    AUX = "AUX"
+    PART = "PART"
     X = "X"
 
 
 _handlers: dict[str, Callable[[Context], None]] = {}
 
 
-def contextual_rule(word: str):
+def segment_line(line: str) -> list[str]:
+    words = []
+    segments = re.split(r"\s+", line)
+    for seg in segments:
+        words += pycantonese.segment(seg)
+    return words
+
+
+def fix_space(line: str) -> str:
+    """Remove spaces between Han characters and non-Han characters."""
+    line = cjk_pattern.sub("", line)
+    return number_pattern.sub("", line)
+
+
+# Debugging purpose
+pos_file = open('pos.txt', 'w', encoding='utf-8')
+
+def apply_contextual_rules(line: str):
+    ctx = Context()
+    ctx.pos_list = pycantonese.pos_tag(segment_line(line))
+    print(ctx.pos_list, file=pos_file)
+    for ctx.i, (ctx.word, ctx.pos) in enumerate(ctx.pos_list):
+        if ctx.i + 1 < len(ctx.pos_list):
+            ctx.next_word, ctx.next_pos = ctx.pos_list[ctx.i+1]
+        else:
+            ctx.next_word, ctx.next_pos = "", Pos.NONE
+
+        rule_handler = _handlers.get(ctx.word, None)
+        if rule_handler:
+            rule_handler(ctx)
+        ctx.prev_word = ctx.word
+        ctx.prev_pos = ctx.pos
+
+    return fix_space(" ".join(w for w, _ in ctx.pos_list))
+
+
+def contextual_rule(word: str, pos: set[str] = set()):
     """Decorator for registering a contextual typo correction rule."""
 
     def deco(f):
+        if word in _handlers:
+            raise ValueError(f'rule for {word} already exists: {_handlers[word]}')
         _handlers[word] = f
         return f
     return deco
@@ -54,6 +102,11 @@ def _(c: Context):
     if c.prev_pos == Pos.VERB:
         c.replace_word("咗")
 
+@contextual_rule("左一")
+def _(c: Context):
+    """左 -> 咗: 如果 左 字前面係一個動詞，噉就改成 咗."""
+    if c.prev_pos == Pos.VERB:
+        c.replace_word("咗一")
 
 @contextual_rule("黎")
 def _(c: Context):
@@ -61,13 +114,11 @@ def _(c: Context):
     if c.pos == Pos.VERB:
         c.replace_word("嚟")
 
-
 @contextual_rule("野")
 def _(c: Context):
     """野 -> 嘢: 如果係隻名詞，就改成 嘢. 包埋動詞同X係因為 pycantonese 有時會識別成動詞."""
     if c.pos in (Pos.PRON, Pos.NOUN, Pos.X) or c.prev_pos == Pos.VERB:
         c.replace_word("嘢")
-
 
 @contextual_rule("既")
 def _(c: Context):
@@ -79,30 +130,65 @@ def _(c: Context):
         return
     c.replace_word("嘅")
 
-# TODO: add more rules from main.py.
+@contextual_rule("咁")
+def _(c: Context):
+    """咁 -> 噉: 如果前面係形容詞、副詞，或者後面後動詞、名詞、代詞，就係 噉"""
+    if c.next_word == '':
+        c.replace_word("噉")
+    if c.next_pos in (Pos.ADJ, Pos.ADV):
+        return
+    if (c.next_pos in (Pos.VERB, Pos.NOUN, Pos.PRON, Pos.PART, Pos.AUX) or
+        c.prev_pos in (Pos.ADJ, Pos.ADV)):
+        c.replace_word("噉")
 
+@contextual_rule("甘")
+def _(c: Context):
+    """甘 -> 咁/噉
+    如果前面係形容詞、副詞，或者後面後動詞、名詞、代詞，就係 噉.
+    如果後面係形容詞、副詞，就係 咁
+    """
+    if c.next_word == '':
+        c.replace_word("噉")
+    if c.pos in (Pos.VERB, Pos.NOUN):
+        return
+    if c.next_pos in (Pos.ADV, Pos.ADJ):
+        c.replace_word("咁")
+        return
+    if (c.next_pos in (Pos.VERB, Pos.NOUN, Pos.PRON, Pos.PART, Pos.AUX) or
+        c.prev_pos in (Pos.ADJ, Pos.ADV)):
+        c.replace_word("噉")
 
-def segment_line(line: str) -> list[str]:
-    words = []
-    segments = re.split("\s+", line)
-    for seg in segments:
-        words += pycantonese.segment(seg)
-    return words
+@contextual_rule("比")
+def _(c: Context):
+    """比 -> 畀: 如果後面第一個詞係名詞，且第二個詞係形容詞、副詞，就係 比"""
+    remain_words, remain_pos = zip(*c.pos_list[c.i:])
+    if "仲" in remain_words or "更" in remain_words or Pos.ADJ in remain_pos or Pos.ADV in remain_pos:
+        return
+    c.replace_word("畀")
 
+@contextual_rule("俾")
+def _(c: Context):
+    c.replace_word("畀")
 
-def apply_contextual_rules(line: str):
-    ctx = Context()
-    ctx.pos_list = pycantonese.pos_tag(segment_line(line))
-    for ctx.i, (ctx.word, ctx.pos) in enumerate(ctx.pos_list):
-        ctx.prev_word = ctx.word
-        ctx.prev_pos = ctx.pos
-        if ctx.i + 1 < len(ctx.pos_list):
-            ctx.next_word, ctx.next_word = ctx.pos_list[ctx.i+1]
-        else:
-            ctx.next_word, ctx.next_word = '', Pos.NONE
+@contextual_rule("曬")
+def _(c: Context):
+    if c.pos == Pos.VERB:
+        c.replace_word("晒")
 
-        rule_handler = _handlers.get(ctx.word, None)
-        if rule_handler:
-            rule_handler(ctx)
+@contextual_rule("無")
+def _(c: Context):
+    if c.next_pos in (Pos.NOUN, Pos.ADP):
+        c.replace_word("冇")
 
-    return ''.join(w for w, _ in ctx.pos_list)
+@contextual_rule("d")
+@contextual_rule("D")
+def _(c: Context):
+    if re.search(cjk_regex, c.next_word) or c.prev_pos in (Pos.ADJ, Pos.ADV):
+        c.replace_word("啲")
+
+@contextual_rule("宜家")
+def _(c: Context):
+    line = "".join(w for w, _ in c.pos_list)
+    if re.search("傢俬|傢俱|家居", line):
+        return
+    c.replace_word("而家")
